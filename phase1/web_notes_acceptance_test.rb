6eac8ad1eeacffec8b5f6dfa7ded9e661d39ba5d
Josh Cheek <josh.cheek@gmail.com>
2016-03-16 08:45:45 -0500

    Acceptance test that passes the rubocop file

--- phase1/web_notes_acceptance_test.rb
+++ phase1/web_notes_acceptance_test.rb
@@ -1,14 +1,15 @@
 require 'notes/web' # <-- you'll need to make this
 require 'net/http'  # this is from the stdlib
 
-class AcceptanceTest < Minitest::Test
+class WebAcceptanceTest < Minitest::Test
   def port
     9292
   end
 
   def run_server(port, app, &block)
     server = Notes::Web.new(app, Port: port, Host: 'localhost')
-    # The thread allows the server to sit and wait for a request, but still return to here so we can send it.
+    # The thread allows the server to sit and wait for a request,
+    # but still return to here so we can send it.
     thread = Thread.new do
       Thread.current.abort_on_exception = true
       server.start
@@ -23,21 +24,20 @@ class AcceptanceTest < Minitest::Test
   def wait_for(thread)
     loop do
       break if thread.status == 'sleep' # it is ready for our request
-      raise "The server did not wait for our request" unless thread.status
+      raise "The iserver finished without waiting for our request" unless thread.status
       Thread.pass
     end
   end
 
-
   def test_it_accepts_and_responds_to_a_web_request
     path_info = "this value should be overridden by the app!"
-
     app = Proc.new do |env_hash|
       path_info = env_hash['PATH_INFO']
-      body      = "hello, class ^_^"
-      [200, {'Content-Type' => 'text/plain', 'Content-Length' => body.length, 'omg' => 'bbq'}, [body]]
+      [ 200,
+        {'Content-Type' => 'text/plain', 'Content-Length' => 16, 'omg' => 'bbq'},
+        ["hello, class ^_^"]
+      ]
     end
-
     run_server port, app do
       response = Net::HTTP.get_response('localhost', '/lolol', port)
       assert_equal "200",              response.code
@@ -47,16 +47,13 @@ class AcceptanceTest < Minitest::Test
     end
   end
 
-
   def test_it_handles_multiple_requests
     app = Proc.new { |env_hash| [200, {'Content-Type' => 'text/plain'}, []] }
-
     run_server port, app do
       assert_equal "200", Net::HTTP.get_response('localhost', '/', port).code
       assert_equal "200", Net::HTTP.get_response('localhost', '/', port).code
     end
   end
-
 
   def test_it_starts_on_the_specified_port
     other_port = 9293
@@ -66,7 +63,6 @@ class AcceptanceTest < Minitest::Test
         ['hello']
       ]
     end
-
     run_server other_port, app do
       assert_equal 'hello', Net::HTTP.get_response('localhost', '/', other_port).body
     end
